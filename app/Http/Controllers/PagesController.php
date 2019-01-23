3e0bf6b326e9616b4506e953bb70f5f95ad10d87
Dorian <dorian.monnet@viacesi.fr>
2019-01-23 11:04:48 +0100

    Ecommerce v.2
    -Ajout des catégories
    -AJout des pages catégorie
    -Ajouts des boutons de panier et du panier
    -Préparation des pages profile et panier

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -20,10 +20,21 @@ class PagesController extends Controller
     }
     public function ecom(){
         $stock = DB::connection('mysql2')->table('stock')->get();
+        $categories = DB::connection('mysql2')->table('categories')->get();
         
-        
-        return view('pages.ecom')->with(array('stock'=>$stock));
+        return view('pages.ecom')->with(array('stock'=>$stock, 'categories'=>$categories));
     }
+    
+    public function catecom($idcat){
+        $stock = DB::connection('mysql2')->table('stock')->where('ID_Categories', '=', $idcat)->get();
+        $categories = DB::connection('mysql2')->table('categories')->where('ID', '=', $idcat)->get();
+        //dump($categories)
+        return view('pages.catecom')->with(array('stock'=>$stock, 'categories'=>$categories));
+;
+    }
+    
+    
+    
     public function event(){
         
         $events = DB::connection('mysql2')->table('events')->get();
--- resources/views/pages/catecom.blade.php
+++ resources/views/pages/catecom.blade.php
@@ -1,30 +1,11 @@
 @extends('layouts.header')
 
 @section('content')
-<div class = "container">
-<div class="col-sm-1 colsm-push-11">
-<button type="button" class="btn btn-primary col-sm-push8" >Panier</button>
-</div>
-</div>
 
 <div class="container" style="border:1px solid #cecece;">
-    <h3>Liste des catégories</h3>
-  @foreach ($categories as $categories)
-    <div class="col-sm-4" href="cat/{{$categories->ID}}">
-        <img src="{{$categories->IMG_URL}}" alt="..." height="180" width="320"/>
-           <div class="wrapper">
-                <div class="caption post-content col-sm-6">
-                    <a href="cat/{{$categories->ID}}">
-                    <h3>{{$categories->TITLE}}</h3>
-                    </a>
-                </div>
-            </div>
-        </div>
+    @foreach ($categories as $categories)
+    <h3>{{$categories->TITLE}}</h3>
     @endforeach
-</div>
-
-<div class="container" style="border:1px solid #cecece;">
-    <h3>Liste des produits</h3>
   @foreach ($stock as $produit)
     <div class="col-sm-4">
         <img src="{{$produit->IMG_URL}}" alt="..." height="180" width="320"/>
