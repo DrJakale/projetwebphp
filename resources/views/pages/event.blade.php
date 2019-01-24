29627a7e7e44c980eef4dd15c1a7e7edf33d9d2f
Louis <loulesko@hotmail.fr>
2019-01-24 13:56:41 +0100

    Event CSS
    Ajout du CSS sur la page event

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
 <div class = "container">
-    <div class="col-sm-1 colsm-push-11">
-        <a style = "margin-bottom:10px" type="button" class="btn btn-primary col-sm-push8" href = "createevent" >Créer évenement</a>
-    </div>
+        <a href="createevent" class="btnevent" role="button">Créer évenement</a>
 </div>
-<div class="container" style="border:1px solid #cecece;">>
+<div class="container" style="border:1px solid #cecece;">
   @foreach ($events as $event)
-<a href="event/{{$event->ID}}">
+  <a href="event/{{$event->ID}}">
     <div class="col-sm-4">
-        <img src="{{$event->URL_Thumbnail}}" alt="..." height="180" width="320"/>
-           <div class="wrapper">
-                <div class="caption post-content">
-                    <h3>{{$event->TITLE}}</h3>
+        <img class="imgevent"src="{{$event->URL_Thumbnail}}" alt="..." height="180" width="320"/>
+        <div class="wrapper">
+            <div class="caption post-content">
+                <h3 class="titlevent">{{$event->TITLE}}</h3>
+                <div class="dscevent">
                     <p>Lorem ipsum dolor sit amet</p>
                 </div>
             </div>
         </div>
-    </a>
-    @endforeach
+    </div>
+</a>
+@endforeach
 </div>
 @endsection
